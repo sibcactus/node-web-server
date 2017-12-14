7cfc261ac883030736b724097eac051ce1ece23e
Nikolay Makarov <nikolaymr@yandex.ru>
2017-12-14 20:41:13 +0300

    remove maint

--- server.js
+++ server.js
@@ -24,10 +24,7 @@ app.use((req, res, next) => {
   });
   next();
 })
-app.use((req,res,next) => {
-  res.render('maint.hbs');
-  next();
-} );
+
 
 app.use(express.static(__dirname + "/public"));
 app.get("/", (req, res) => {
